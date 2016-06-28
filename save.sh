48fe7de5d95d3bc7410d8dd6bcf4be53bfb27f8c
Hoseung Choi <hopung@gmail.com>
2016-06-28 14:43:28 +0900

    tmp

--- save.sh
+++
@@ -1,8 +0,0 @@
-git add -u   # stage all changes / new files in the directory
-## git status
-git commit -m 'auto_saved'
-git push -u origin master
-
-
-#  Force this version to be the master (overwrite)
-#  git push -f origin master
--- send_pycluster.sh
+++
@@ -1,8 +0,0 @@
-#rsync -av --progress /home/hoseung/Copy/pyclusterevol/* hoseung@165.132.29.74:~/Copy/pyclusterevol/
-rsync -av --progress ./galaxy ./tree ./scripts ./draw ./utils hoseung@165.132.29.71:~/Work/pyclusterevol/
-#rsync -av --progress ./galaxy ./tree ./scripts ./draw ./utils hoseung@165.132.29.66:~/Work/pyclusterevol/
-#rsync -av --progress ./galaxy ./load ./scripts ./draw ./tree ./utils ./zoom  hoseung@165.132.29.74:~/Work/pyclusterevol/ 
-#rsync -av --progress  ./galaxy ./load ./scripts ./draw ./tree ./utils ./zoom  hoseung@165.132.35.192:/media/storage/hoseung/pyclusterevol/
-#rsync -av --progress  ./galaxy ./load ./scripts ./draw ./tree ./utils ./zoom  g088chs@tachyon2.ksc.re.kr:~/Work/pyclusterevol/ 
-#rsync -av --progress ./load ./scripts ./draw ./tree ./utils ./zoom  x976chs@tachyon2.ksc.re.kr:~/Work/pyclusterevol/ 
-#rsync -av --progress  ../pyclusterevol  hoseung@165.132.29.66:~/Work/pyclusterevol/
