domain calculation fixed. calculated by python methods, not by fortran subroutines

load/hydro.py
@@ -61,29 +61,6 @@ class Hydro(load.sim.Simbase):
     def set_info(self, info):
         self.info = info
 
-
-    def set_ranges(self, ranges=None):
-        if ranges is None:
-            ranges = self.info.ranges
-        nr = np.asarray(ranges)
-        if not(nr.shape[0] == 3 and nr.shape[1] == 2):
-            # Because actual operation on the given input(ranges)
-            # does not take place soon, it's not a good place to use
-            # try & except clause. There is nothing to try yet.           
-            raise ValueError('Shape of ranges is wrong: {} \n example : [[0.1,0.3],[0.2,0.4],[0.6,0.8]] \n'.format(nr.shape))
-        else:
-            self.info.ranges = ranges
-            self.set_cpus(self._hilbert_cpulist(self.info, self.info.ranges))
-
-
-    def set_cpus(self, cpus):
-        self.cpus = cpus
-        try:
-            print("Updating info.cpus")
-            self.info._set_cpus(self.get_cpus())
-        except AttributeError:
-            print("No info._set_cpus attribute??")
-
     def _read_hydro_header(self, f, verbose=False):
         # Global
         h1 = read_header(f,
@@ -133,25 +110,24 @@ class Hydro(load.sim.Simbase):
 
         if verbose: print(' >>> working resolution (lmax) =', lmax)
 
+        if ranges is not None: self.set_ranges(ranges=ranges)
         # Set ranges
         xmi, xma = self.info.ranges[0]
-#        xma = self.info.ranges[0][1]
         ymi, yma = self.info.ranges[1]
-#        yma = self.info.ranges[1][1]
         zmi, zma = self.info.ranges[2]
-#        zma = self.info.ranges[2][1]
+
         work_dir = self.info.base + '/' + self.out_dir + 'output_' + str(self.info.nout).zfill(5)
-        if verbose: print(xmi, xma, ymi, yma, zmi,zma)
-        out = a2c.a2c_count(work_dir, xmi, xma, ymi, yma, zmi, zma, lmax)
-        # I want to calculate domain in python,
-        # so that a2c only does loading part.
-        #if xmi > 0:
-        #    print(
+        if verbose:
+            print("[hydro.amr2cell] Ranges", xmi, xma, ymi, yma, zmi,zma)
+            print("[hydro.amr2cell] cpus", self.cpus)
+            
+        out = a2c.a2c_count(work_dir, xmi, xma, ymi, yma, zmi, zma, lmax, self.cpus)
+
         if return_meta:
             return (out[0], work_dir, xmi, xma, ymi, yma, zmi, zma, lmax)
         else:
             cell = a2c.a2c_load(work_dir, xmi, xma, ymi, yma, zmi, zma,\
-                                lmax, out[0], nvarh)
+                                lmax, out[0], nvarh, self.cpus)
             dtype_cell = [('x', '<f8'), ('y', '<f8'), ('z', '<f8'), ('dx', '<f8')] 
             if cpu:
                 dtype_cell.append(('cpu', '<f8'))
@@ -171,217 +147,4 @@ class Hydro(load.sim.Simbase):
 #                                       cell[2][:,0], cell[2][:,1], cell[2][:,2],
 #                                       cell[2][:,3], cell[2][:,4], cell[2][:,5]],
 #                                       dtype = dtype_cell)
-
-
-    def amr2cell_old(self, lmax=None, icpu=0, verbose=False):
-        """
-        Reads AMR and HYDRO and output hydro variables in particle-like data format.
-        Works ok.
-
-        Initial version: 2015. 04. 16
-        """
-        import numpy as np
-        # if both amr and hydro does not exist, create.
-        #################
-        cpus = self.info.cpus
-        ndim = self.info.ndim
-        print("CPU list:", cpus)
-        twotondim = 2**ndim
-        nvarh = self.header.nvarh
-        ncpu = self.header.ncpu
-        nboundary = self.header.nboundary
-        nlevelmax = self.header.nlevelmax
-        if lmax is None:
-            lmax = nlevelmax
-
-        print(' >>> working resolution (lmax) =', lmax)
-
-        # Set ranges
-        xmi = self.info.ranges[0][0]
-        xma = self.info.ranges[0][1]
-        ymi = self.info.ranges[1][0]
-        yma = self.info.ranges[1][1]
-        zmi = self.info.ranges[2][0]
-        zma = self.info.ranges[2][1]
-
-        # Size of arrays
-        if icpu == 0:
-            listmax = ncpu
-        elif icpu > 0:
-            listmax = ncpu + nboundary
-        ngridarr = np.zeros((nlevelmax, listmax), dtype=np.int32)
-
-        nx = self.amr.header.ng[0]
-        ny = self.amr.header.ng[1]
-        nz = self.amr.header.ng[2]
-        xbound = [int(nx/2), int(ny/2), int(nz/2)]
-
-        # Initialize pointer array
-
-        nlinemax = 10000000
-        xt = np.zeros(nlinemax, dtype=np.float64)
-        yt = np.zeros(nlinemax, dtype=np.float64)
-        zt = np.zeros(nlinemax, dtype=np.float64)
-        dxt = np.zeros(nlinemax, dtype=np.float64)
-        vart = np.zeros((nvarh, nlinemax), dtype=np.float64)
-
-        nline = 0
-        for icpu in cpus:
-            print("reading", icpu)
-            # Open AMR file and read header
-            famr = open(self.amr._fnbase + str(icpu).zfill(5), "rb")
-
-            header_icpu = load.amr.AmrHeader()
-            header_icpu._read_amr_header(famr)
-
-            # This is important ??
-            ngridarr[0: nlevelmax][0: ncpu + nboundary] = header_icpu.numbl
-#            if nboundary > 0:
-#          ngridarr[0: nlevelmax][ncpu: ncpu + nboundary] = header_icpu.numbb
-
-            # because hydro file is written in fortran order
-            # looping over
-            # for ilevel
-            #   for icpu
-            # Open Hydro file and skip header
-
-            fhydro = open(self._fnbase + str(icpu).zfill(5), "rb")
-            self._read_hydro_header(fhydro)
-
-            xc = np.zeros((3, 8), dtype=np.float64)
-
-            for ilevel in range(1, lmax + 1):
-                # geometry
-                dx = 0.5**ilevel  # grid size of the current level.
-                dx2 = dx/2  # half of the grid size of the current level.
-                nx_full = 2**ilevel  # maximum possible number of grid cells at the given level.
-                ny_full = 2**ilevel
-                nz_full = 2**ilevel
-                # division results in floats!
-                for ioct in range(twotondim):  #  1 to 8
-                    iz = int((ioct) / 4)
-                    iy = int((ioct - 4 * iz) / 2)
-                    ix = int((ioct - 2 * iy - 4 * iz))
-                    xc[0][ioct] = (ix - 0.5) * dx  # grid center
-                    xc[1][ioct] = (iy - 0.5) * dx
-                    xc[2][ioct] = (iz - 0.5) * dx
-
-                # Allocate work arrays
-                ngrida = ngridarr[ilevel-1][icpu-1]
-
-                if ngrida > 0:
-                    # note that index stars from 0 to reduce memory use
-                    xg = np.zeros((ndim, ngrida), dtype=np.float64)
-                    son = np.zeros((twotondim, ngrida), dtype=np.int32)
-                    var = np.zeros((nvarh, twotondim, ngrida,),
-                                   dtype=np.float64)
-                    x = np.zeros((ndim, ngrida), dtype=np.float64)
-#                    rho = np.zeros(ngrida, dtype=np.float64)
-
-# Main loop, loop over doamin
-                for jcpu in range(1, nboundary + ncpu + 1):
-                    ng = ngridarr[ilevel-1][jcpu-1]
-                    if ng > 0: # If ng == 0, go straight to Read Hydro data
-                        if (verbose):
-                            print("Level {:2d} has {:6d} grids in proc {:4d}".format(ilevel, ng, jcpu))
-# SKIP
-                        read_fortran(famr, np.int32, ng)  # grid index
-                        read_fortran(famr, np.int32, ng)  # next index
-                        read_fortran(famr, np.int32, ng)  # prev index
-# Read gird center
-                        for idim in range(ndim):
-                            if jcpu == icpu:
-                                xtemp = read_fortran(famr, np.dtype('f8'), ng)
-                                xg[idim][:] = xtemp
-                            else:
-                                read_fortran(famr, np.dtype('f8'), ng)
-# skip father index
-                        read_fortran(famr, np.int32, ng)
-# skip neighbour index ( 6 possible neighbours )
-                        for idim in range(2*ndim):
-                            read_fortran(famr, np.int32, ng)
-# Read son index ( 8 possible children, oct-tree )
-                        for idim in range(2**ndim):
-                            if jcpu == icpu:
-                                son[idim][:] = read_fortran(famr, np.int32, ng)
-                            else:
-                                read_fortran(famr, np.int32, ng)
-# skip cpu map
-                        for idim in range(2**ndim):
-                            read_fortran(famr, np.dtype('i4'), ng)
-# skip ref map
-                        for idim in range(2**ndim):
-                            read_fortran(famr, np.dtype('i4'), ng)
-# Read Hydro data
-# Skip Hydro header always.
-                    skip_fortran(fhydro)
-                    skip_fortran(fhydro)
-
-                    if ng > 0:
-                        for ioct in range(twotondim):
-                            for ivar in range(nvarh):
-                                if jcpu == icpu:
-                                    var[ivar][ioct][:] = read_fortran(fhydro, np.dtype('f8'), ng)
-                                else:
-                                    read_fortran(fhydro, np.dtype('f8'), ng)
-# Compute map
-                if ngrida > 0:
-                    for ioct in range(twotondim):
-                        for idim in range(3):
-                            x[idim][:] = xg[idim][:] + xc[idim][ioct] - xbound[idim]
-
-                        ref = np.zeros(ngrida, dtype=np.int16)
-                        if ilevel < lmax:
-                            val = np.where(son[ioct][:] > 0)[0]
-                            if len(val) > 0:
-                                ref[val] = 1
-                        ok_cell = np.where((ref == 0) &
-                                           (x[0, :] + dx2 >= xmi) &
-                                           (x[1, :] + dx2 >= ymi) &
-                                           (x[2, :] + dx2 >= zmi) &
-                                           (x[0, :] - dx2 <= xma) &
-                                           (x[1, :] - dx2 <= yma) &
-                                           (x[2, :] - dx2 <= zma))[0]
-
-#################  It is extremely slow!!!   Use np.where instead.
-#                            ok_cell = np.all([ref == 0,
-#                                              x[0, :] + dx2 >= xmi, x[0, :] - dx2 <= xma,
-#                                              x[1, :] + dx2 >= ymi, x[1, :] - dx2 <= yma,
-#                                              x[2, :] + dx2 >= zmi, x[2, :] - dx2 <= zma],
-#                                              axis=0)
-
-                        Nok_cell = len(ok_cell)
-                        if Nok_cell > 0:
-                            nbegin = nline
-                            nend = nline + Nok_cell
-
-                            # If the existing arrays are not large enough.
-                            if nend > nlinemax:
-                                print(nend, " > ", nlinemax)
-                                xt = np.append(xt, np.zeros(nlinemax, dtype=np.float64))
-                                yt = np.append(yt, np.zeros(nlinemax, dtype=np.float64))
-                                zt = np.append(zt, np.zeros(nlinemax, dtype=np.float64))
-                                dxt = np.append(dxt, np.zeros(nlinemax, dtype=np.float64))
-                                vart = np.append(vart, np.zeros((nvarh,nlinemax), dtype=np.float64), axis=1)
-                                nlinemax += nlinemax
-# save variables
-                            xt[nbegin:nend] = x[0, ok_cell]
-                            yt[nbegin:nend] = x[1, ok_cell]
-                            zt[nbegin:nend] = x[2, ok_cell]
-                            dxt[nbegin:nend] = dx
-                            vart[:, nbegin:nend] = var[:, ioct, ok_cell]
-                            nline = nend
-
-            # loop over ilevel
-            famr.close()
-            fhydro.close()
-
-        dtype_cell = [('x', '<f8'), ('y', '<f8'), ('z', '<f8'),('dx', '<f8'),
-                      ('var0', '<f8'), ('var1', '<f8'), ('var2', '<f8'),
-                        ('var3', '<f8'), ('var4', '<f8'), ('var5', '<f8')]
-
-        self.cell = np.rec.fromarrays([xt[:nend], yt[:nend], zt[:nend], dxt[:nend],
-                                       vart[0][:nend], vart[1][:nend], vart[2][:nend],
-                                       vart[3][:nend], vart[4][:nend], vart[5][:nend]],
-                                       dtype = dtype_cell)
 

load/info.py
@@ -67,7 +67,7 @@ class Info:
         self.ranges = ranges
 
     def _set_cpus(self, cpus):
-        self.cpus = cpus
+        self.cpus = np.array(cpus)
 
     def set_fn(self, fn, verbose=False):
         self.fn = fn
@@ -175,7 +175,7 @@ class Info:
                 arr2.append(float(str.split(f.readline(), '=')[1]))
             self._cal_units(arr1, arr2)
             self.hilbertkey = np.zeros((2, self.ncpu_tot + 1),
-                                       dtype=np.float32)
+                                       dtype=np.float64)
             f.readline()
             f.readline()
             f.readline()

load/part.py
@@ -139,20 +139,6 @@ class Part(load.sim.Simbase):
             if max(part["m"]) < 100:
                 part["m"] *= self.info.msun
 
-    def set_ranges(self, ranges=None):
-        if ranges is None:
-            ranges = self.ranges
-        nr = np.asarray(ranges)
-        if not(nr.shape[0] == 3 and nr.shape[1] == 2):
-            # Because actual operation on the given input(ranges)
-            # does not take place soon, it's not a good place to use
-            # try & except clause. There is nothing to try yet.
-            print(' Error!')
-            print('Shape of ranges is wrong:', nr.shape)
-            print('example : [[0.1,0.3],[0.2,0.4],[0.6,0.8]] \n')
-        else:
-            self.ranges = ranges
-            self.set_cpus(self._hilbert_cpulist(self.info, self.ranges))
 
     def set_base(self, base):
         """
@@ -169,14 +155,6 @@ class Part(load.sim.Simbase):
         from os import path
         snout = str(self.info.nout).zfill(5)
         self._fbase = path.abspath(path.join(self.base, data_dir +'output_' + snout + '/part_' + snout + '.out'))
-
-    def set_cpus(self, cpus):
-        self.cpus = cpus
-        try:
-            print("Updating info.cpus")
-            self.info._set_cpus(self.get_cpus())
-        except AttributeError:
-            print("No info._set_cpus attribute??")
 
     def setwhattoread(self, ptypes):
         """
@@ -680,10 +658,9 @@ class Part(load.sim.Simbase):
         zmi = self.ranges[2][0]
         zma = self.ranges[2][1]
         work_dir = self.info.base + '/snapshots/output_' + str(self.info.nout).zfill(5)
-        print("part.cpus", self.cpus)
         ndm_actual, nstar_actual, nsink_actual = part_shared.count_part( \
                             work_dir, xmi, xma, ymi, yma, zmi, zma, self.cpus)
-        print(ndm_actual, nstar_actual, nsink_actual)
+        #print(ndm_actual, nstar_actual, nsink_actual)
         self.ndm = ndm_actual
         self.nstar = nstar_actual
         self.nsink = nsink_actual

load/sim.py
@@ -7,12 +7,13 @@ Created on Tue Jan 13 23:27:16 2015
 
 import numpy as np
 
+
+
 class Simbase():
     """ 
     base    
     """
     def __init__(self):
-        
         pass
     
     def add_amr(self):
@@ -20,13 +21,45 @@ class Simbase():
         self.amr = amr.Amr(self.info)
         print("An AMR instance is created\n")
 
+    def get_cpus(self):
+        return self.cpus
+
+    def set_cpus(self, cpus):
+        self.cpus = np.array(cpus)
+        #print("Updating info.cpus")
+        #self.info._set_cpus(self.get_cpus())
+
+    def show_cpus(self):
+        print(" ncpus : %s \n" % self.get_cpus())
+
+    def set_ranges(self, ranges=[[0, 1], [0, 1], [0, 1]]):
+        nr = np.asarray(ranges)
+        if not(nr.shape[0] == 3 and nr.shape[1] == 2):
+            # Because actual operation on the given input(ranges)
+            # does not take place soon, it's not a good place to use
+            # try & except clause. There is nothing to try yet.
+            print(' Error!')
+            print('Shape of ranges is wrong:', nr.shape)
+            print('example : [[0.1,0.3],[0.2,0.4],[0.6,0.8]] \n')
+        else:
+            self.ranges = ranges
+            try:
+                self.info._set_ranges(self.ranges)
+            except AttributeError:
+                print("There is no info._set_ranges attribute")
+            self.set_cpus(self._hilbert_cpulist(self.info, self.ranges))
+
+    
     def _hilbert_cpulist(self, info, ranges):
         """
-        After determining cpu numbers, read the cpu files and cut off data
+        After determining cpus, read the cpu files and cut off data
         that are outside ranges.
         -> cpu files contain data points within a given ragnes
         BUT! they also contain other data points outside the ragnes.
         """
+
+        from load.a2c import hilbert3d
+
         if not(hasattr(self, 'amr')):
             print("No AMR instance,")
             print("Loading one...")
@@ -68,7 +101,7 @@ class Simbase():
         dmax = max([xxmax-xxmin, yymax-yymin, zzmax-zzmin])
         for ilevel in range(1, lmax):
             dx = 0.5**ilevel
-            if (dx <= dmax):
+            if (dx < dmax):
                 lmin = ilevel
                 break
 
@@ -81,7 +114,7 @@ class Simbase():
         kmin = 0
         kmax = 0
 
-        if (bit_length >= 0):
+        if (bit_length > 0):
             imin = int(xxmin * maxdom)
             imax = imin + 1
             jmin = int(yymin * maxdom)
@@ -91,24 +124,31 @@ class Simbase():
 
         dkey = (2**(nlevelmax+1)/maxdom)**(ndim)
         ndom = 1
-        if (bit_length > 0):
-            ndom = 8
+        if (bit_length > 0): ndom = 8
         idom = np.zeros(9)
         jdom = np.zeros(9)
         kdom = np.zeros(9)
-        idom[1] = imin; idom[2] = imax; idom[3] = imin; idom[4] = imax
-        idom[5] = imin; idom[6] = imax; idom[7] = imin; idom[8] = imax
-        jdom[1] = jmin; jdom[2] = jmin; jdom[3] = jmax; jdom[4] = jmax
-        jdom[5] = jmin; jdom[6] = jmin; jdom[7] = jmax; jdom[8] = jmax
-        kdom[1] = kmin; kdom[2] = kmin; kdom[3] = kmin; kdom[4] = kmin
-        kdom[5] = kmax; kdom[6] = kmax; kdom[7] = kmax; kdom[8] = kmax
+        idom[1] = imin; idom[2] = imax
+        idom[3] = imin; idom[4] = imax
+        idom[5] = imin; idom[6] = imax
+        idom[7] = imin; idom[8] = imax
+        jdom[1] = jmin; jdom[2] = jmin
+        jdom[3] = jmax; jdom[4] = jmax
+        jdom[5] = jmin; jdom[6] = jmin
+        jdom[7] = jmax; jdom[8] = jmax
+        kdom[1] = kmin; kdom[2] = kmin
+        kdom[3] = kmin; kdom[4] = kmin
+        kdom[5] = kmax; kdom[6] = kmax
+        kdom[7] = kmax; kdom[8] = kmax
 
         bounding_min = np.zeros(9)
         bounding_max = np.zeros(9)
 
         for i in range(1, ndom + 1):
             if bit_length > 0:
-                order_min = self._hilbert3d([idom[i]], [jdom[i]], [kdom[i]],
+                #order_min = self._hilbert3d([idom[i]], [jdom[i]], [kdom[i]],
+                #                            bit_length, 1)
+                order_min = hilbert3d([idom[i]], [jdom[i]], [kdom[i]],
                                             bit_length, 1)
                 # order_min, array or single variable??
                 # Will it be vectorized??
@@ -129,10 +169,10 @@ class Simbase():
         for impi in range(1, ncpu + 1):
             for i in range(1, ndom + 1):
                 if ((bound_key[impi - 1] <= bounding_min[i]) and
-                   (bound_key[impi] > bounding_min[i])):
+                    (bound_key[impi    ] >  bounding_min[i])):
                     cpu_min[i] = impi
-                if ((bound_key[impi - 1] < bounding_max[i]) and
-                   (bound_key[impi] >= bounding_max[i])):
+                if ((bound_key[impi - 1] <  bounding_max[i]) and
+                    (bound_key[impi    ] >= bounding_max[i])):
                     cpu_max[i] = impi
 
         ncpu_read = 0
@@ -145,23 +185,14 @@ class Simbase():
                     cpu_list[ncpu_read] = j
                     cpu_read[j] = 1
 
-        for ilevel in range(1, lmax + 1):
-            nx_full = 2**ilevel
-            ny_full = 2**ilevel
-            nz_full = 2**ilevel
-            imin = xxmin * (nx_full) + 1
-            imax = xxmax * (nx_full) + 1
-            jmin = yymin * (ny_full) + 1
-            jmax = yymax * (ny_full) + 1
-            kmin = zzmin * (nz_full) + 1
-            kmax = zzmax * (nz_full) + 1
-
 # Sort cpu_list in descending npart order for memory efficiency
         cpu_list = cpu_list[cpu_list > 0]  # crop empty part
 
         return np.sort(cpu_list)
 
-    def _hilbert3d(self, x, y, z, bit_length, npoint):
+
+
+    def deprecated_hilbert3d(self, x, y, z, bit_length, npoint):
         '''
         Calculate hilbert doamin decomposition
         '''
@@ -309,7 +340,6 @@ class Sim(Simbase):
         # info appreciates nout and base (not mandatary, though)
         self.dmo = dmo
         # DMO runs are slightly different!
-        #print("data_dir =", data_dir)
         self.set_data_dir(data_dir)
         self.add_info()
         # set_data_dir and set_range needs info instance be exist.
@@ -416,41 +446,6 @@ class Sim(Simbase):
         else:
             print("Use part.load() to load particle")
 
-    def get_cpus(self):
-        return self.cpus
-
-    def set_cpus(self, cpus):
-        self.cpus = cpus
-        try:
-            print("Updating info.cpus")
-            self.info._set_cpus(self.get_cpus())
-        except AttributeError:
-            print("No info._set_cpus attribute??")
-        #self.show_cpus()
-
-    def show_cpus(self):
-        print(" ncpus : %s \n" % self.get_cpus())
-
-    def set_ranges(self, ranges=[[0, 1], [0, 1], [0, 1]]):
-        nr = np.asarray(ranges)
-        if not(nr.shape[0] == 3 and nr.shape[1] == 2):
-            # Because actual operation on the given input(ranges)
-            # does not take place soon, it's not a good place to use
-            # try & except clause. There is nothing to try yet.
-            print(' Error!')
-            print('Shape of ranges is wrong:', nr.shape)
-            print('example : [[0.1,0.3],[0.2,0.4],[0.6,0.8]] \n')
-        else:
-            self.ranges = ranges
-            try:
-                self.info._set_ranges(self.ranges)
-            except AttributeError:
-                print("There is no info._set_ranges attribute")
-            self.show_ranges()
-            self.set_cpus(self._hilbert_cpulist(self.info, self.ranges))
-
-    def show_ranges(self):
-        print("Ranges = %s\n" % self.ranges)
 
     def search_zoomin_region(self, *args, **kwargs):
         """