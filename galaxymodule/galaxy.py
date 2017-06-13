36650cd18ac28f70596d896d55e77b876c64edc2
Hoseung <hopung@gmail.com>
2017-06-13 15:33:30 +0900

    galaxy.py gcat[r] from tree

--- galaxymodule/galaxy.py
+++ galaxymodule/galaxy.py
@@ -133,6 +133,7 @@ class Galaxy():
               	      	   	  	vx=tt["vp"][0],
               	      	   	  	vy=tt["vp"][1],
               	      	        vz=tt["vp"][2],
+                                r=tt["r"],
               	      	   	  	m=tt["m"]*1e11,
               	      	        id=tt["id"],
                                 mvir=tt["mvir"],
