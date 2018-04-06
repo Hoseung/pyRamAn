ad29c86a497a3c4c51b1797e22049993364df9a7
Hoseung <hopung@gmail.com>
2018-04-06 18:43:30 +0900

    keep original dtype form and use a function to add alias.

--- load/dtypes.py
+++ load/dtypes.py
@@ -1,3 +1,46 @@
+import numpy as np
+
+def add_dtypes(old_dtypes, add_dtypes):
+    """
+    append new field, or alias to the current dtypes.
+    old_dtypes may be np.dtype or an input to make a np.dtype.
+
+    add_dtypes must be composed of 5 elements:
+        name of the field,
+        data type,
+        shape,
+        reference field name,
+        offset from the reference field name.
+
+    example
+    -------
+    old_dtypes = [('nstep', '<i4'),
+                  ('id', '<i4'),
+                  ('m', dtype_float)]
+
+    add_dtypes = [("x", dtype_float, 1, "xp", 0),
+                  ("y", dtype_float, 1, "xp", 8),
+                  ("z", dtype_float, 1, "xp", 16),
+                  ("vx", dtype_float, 1, "vp", 0),
+                  ("vy", dtype_float, 1, "vp", 8),
+                  ("vz", dtype_float, 1, "vp", 16)]
+
+    new_dt = add_dtypes(old_dtyes, add_dtypes)
+    """
+    if not isinstance(old_dtypes, np.dtype):
+        old_dtypes = np.dtype(old_dtypes)
+
+    dtype_new =dict(old_dtypes.fields)
+
+    for ad in add_dtypes:
+        try:
+            offset = old_dtypes.fields.get(ad[3])[1]
+        except :
+            print("[load/dtype error] Can't find the field ({}) in the old dtypes".format(ad[3]))
+            raise
+        dtype_new.update({ad[0]: ((ad[1], ad[2]), offset+ad[4])})
+
+    return dtype_new
 
 
 def get_halo_dtype(is_gal=False, double=False):
@@ -8,70 +51,59 @@ def get_halo_dtype(is_gal=False, double=False):
         dtype_float = "<f4"
         df=4
 
-    dtype_halo = {'np': (('<i4', 1), 0),
-                  'id': (('<i4', 1), 4),
-                  'level': (('<i4', 1), 8),
-                  'host': (('<i4', 1), 12),
-                  'sub': (('<i4', 1), 16),
-                  'nsub': (('<i4', 1), 20),
-                  'nextsub': (('<i4', 1), 24),
-                  'm': ((dtype_float, 1), 24+df),
-                  'mvir': ((dtype_float, 1), 24+2*df),
-                  'r': ((dtype_float, 1), 24+3*df),
-                  'rvir': ((dtype_float, 1), 24+4*df),
-                  'tvir': ((dtype_float, 1), 24+5*df),
-                  'cvel': ((dtype_float, 1), 24+6*df),
-                  'x': ((dtype_float, 1), 24+7*df),
-                  'y': ((dtype_float, 1), 24+8*df),
-                  'z': ((dtype_float, 1), 24+9*df),
-                  'pos': ((dtype_float, 3), 24+7*df),
-                  'vx': ((dtype_float, 1), 24+10*df),
-                  'vy': ((dtype_float, 1), 24+11*df),
-                  'vz': ((dtype_float, 1), 24+12*df),
-                  'vel': ((dtype_float, 3), 24+10*df),
-                  'm': ((dtype_float, 1), 24+13*df),
-                  'ax': ((dtype_float, 1), 24+14*df),
-                  'ay': ((dtype_float, 1), 24+15*df),
-                  'az': ((dtype_float, 1), 24+16*df),
-                  'lvec': ((dtype_float, 3), 24+14*df),
-                  'sp': ((dtype_float, 1), 24+17*df),
-                  'idx': (('<i4', 1), 24+18*df),
-                  'p_rho': ((dtype_float, 1), 28+18*df),
-                  'p_c': ((dtype_float, 1), 28+19*df),
-                  'energy': ((dtype_float, 3), 28+20*df),
-                  'abc': ((dtype_float, 3), 28+23*df)}
+    dtype_halo = [('np', '<i4'), ('id', '<i4'), ('level', '<i4'),
+                  ('host', '<i4'), ('sub', '<i4'), ('nsub', '<i4'),
+                  ('nextsub', '<i4'),
+                  ('m', dtype_float), ('mvir', dtype_float),
+                  ('r', dtype_float), ('rvir', dtype_float),
+                  ('tvir', dtype_float), ('cvel', dtype_float),
+                  ('x', dtype_float), ('y', dtype_float), ('z', dtype_float),
+                  ('vx', dtype_float), ('vy', dtype_float), ('vz', dtype_float),
+                  ('ax', dtype_float), ('ay', dtype_float), ('az', dtype_float),
+                  ('sp', dtype_float), ('idx', '<i4'),
+                  ('p_rho', dtype_float),('p_c', dtype_float),
+                  ('energy', '<f8', (3,)), ('abc', '<f8', (3,))]
 
     if is_gal:
-        dtype_halo.update({'sig': ((dtype_float, 1), 28+26*df),
-                           'sigbulge': ((dtype_float, 1), 28+27*df),
-                           'mbulge': ((dtype_float, 1), 28+28*df),
-                           'hosthalo': (("<i4", 1), 28+29*df),
-                           'g_nbin': (("<i4", 1), 32+29*df),
-                           'g_rr': ((dtype_float, 100), 36+29*df),
-                           'g_rho': ((dtype_float, 100), 32+129*df)})
-    return dtype_halo
+        dtype_halo += [('sig', dtype_float), ('sigbulge', dtype_float),
+                       ('mbulge', dtype_float), ('hosthalo', '<i4'),
+                       ('g_nbin', '<i4'), ('g_rr', dtype_float, (100,)),
+                       ('g_rho', dtype_float, (100,))]
+
+    add_dtype = [("pos", dtype_float, (3,), "x", 0),
+                 ("vel", dtype_float, (3,), "vx", 0),
+                 ("lvec", dtype_float, (3,), "ax", 0)]
+
+    return add_dtypes(dtype_halo, add_dtype)
 
 
-def get_tree_dtypes(BIG_RUN=False):
+def get_tree_dtypes(BIG_RUN=False, double=True):
+    if double:
+        dtype_float = "<f8"
+        df=8
+    else:
+        dtype_float = "<f4"
+        df=4
+
     dtype_tree = [('nstep', '<i4'),
                   ('id', '<i4'),
-                  ('m', '<f8'),
-                  ('macc', '<f8'),
+                  ('m', dtype_float),
+                  ('macc', dtype_float),
                   ('nsub', '<i4'),
-                  ('xp', '<f8', (3,)),
-                  ('vp', '<f8', (3,)),
-                  ('lp', '<f8', (3,)),
-                  ('abc', '<f8', (4,)),
-                  ("ek", '<f8'),
-                  ("ep", '<f8'),
-                  ("et", '<f8'),
-                  ("spin", '<f8'),
-                  ('mvir', '<f8'),
-                  ('rvir', '<f8'),
-                  ('tvir', '<f8'),
-                  ('cvel', '<f8'),
-                  ('rho_0', '<f8'),
-                  ('rs', '<f8'),
+                  ('xp', dtype_float, (3,)),
+                  ('vp', dtype_float, (3,)),
+                  ('lp', dtype_float, (3,)),
+                  ('abc', dtype_float, (4,)),
+                  ("ek", dtype_float),
+                  ("ep", dtype_float),
+                  ("et", dtype_float),
+                  ("spin", dtype_float),
+                  ('mvir', dtype_float),
+                  ('rvir', dtype_float),
+                  ('tvir', dtype_float),
+                  ('cvel', dtype_float),
+                  ('rho_0', dtype_float),
+                  ('rs', dtype_float),
                   ('level', '<i4'),
                   ('hosthalo', '<i4'), ('hostsub', '<i4'),
                   ('nextsub', '<i4'), ('idx', '<i4'),
@@ -82,26 +114,11 @@ def get_tree_dtypes(BIG_RUN=False):
     if not BIG_RUN:
         dtype_tree.append(("np", '<i4'))
 
-        if True:
-            """
-            This allows multiple ways of accessing fields.
+    add_dtype = [("x", dtype_float, 1, "xp", 0),
+                 ("y", dtype_float, 1, "xp", df),
+                 ("z", dtype_float, 1, "xp", 2*df),
+                 ("vx", dtype_float, 1, "vp", 0),
+                 ("vy", dtype_float, 1, "vp", df),
+                 ("vz", dtype_float, 1, "vp", 2*df)]
 
-            tree["x"] is equivalent with tree["pos"][:,0]
-            tree["vx"] is equivalent with tree["vel"][:,0]
-
-            Doing this requires an optional parameter "offset" to be given,
-            which is calculated as offset+ad[3].
-            """
-            add_dtype = [("x", "f8", 1, 0, "xp"),
-                         ("y", "f8", 1, 8, "xp"),
-                         ("z", "f8", 1, 16, "xp"),
-                         ("vx", "f8", 1, 0, "vp"),
-                         ("vy", "f8", 1, 8, "vp"),
-                         ("vz", "f8", 1, 16, "vp")]
-            dt = np.dtype(dtype_tree)
-            dtype_tree =dict(dt.fields)
-            for ad in add_dtype:
-                offset = dt.fields.get(ad[4])[1]
-                dtype_tree.update({ad[0]: ((ad[1], ad[2]), offset+ad[3])})
-
-    return dtype_tree
+    return add_dtypes(dtype_tree, add_dtype)
