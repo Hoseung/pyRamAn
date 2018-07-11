438029a5f6b48dd8203f84021b3d82e5dfe8dead
HS on desktop <hopung@gmail.com>
2018-07-11 11:14:13 +0900

    functionality to add arbitrary fields on constructing halo catalogs

--- load/dtypes.py
+++ load/dtypes.py
@@ -1,6 +1,17 @@
 import numpy as np
 
-def add_dtypes(old_dtypes, add_dtypes):
+"""
+    functions to manipulate dtypes of numpy arrays.
+
+    NOTE
+    ----
+    Field ordering:
+        Because numpy arrays are mainly accessed by their field name, there's no
+        need to worry about the ordering. Thus, it is enough to put a new field
+        to the end of the existing field list.
+"""
+
+def add_dtypes(old_dtypes, new_dtypes):
     """
     append new field, or alias to the current dtypes.
     old_dtypes may be np.dtype or an input to make a np.dtype.
@@ -14,36 +25,81 @@ def add_dtypes(old_dtypes, add_dtypes):
 
     example
     -------
-    old_dtypes = [('nstep', '<i4'),
+    >>> old_dtypes = [('nstep', '<i4'),
                   ('id', '<i4'),
                   ('m', dtype_float)]
 
-    add_dtypes = [("x", dtype_float, 1, "xp", 0),
+    >>> add_dtypes = [("x", dtype_float, 1, "xp", 0),
                   ("y", dtype_float, 1, "xp", 8),
                   ("z", dtype_float, 1, "xp", 16),
                   ("vx", dtype_float, 1, "vp", 0),
                   ("vy", dtype_float, 1, "vp", 8),
                   ("vz", dtype_float, 1, "vp", 16)]
 
-    new_dt = add_dtypes(old_dtyes, add_dtypes)
+    >>> new_dt = add_dtypes(old_dtyes, add_dtypes)
+
+    If the name of reference field is empty ("" or None),
+    the entry is added to the end of the current field list and the offset is ignored.
+
     """
     if not isinstance(old_dtypes, np.dtype):
         old_dtypes = np.dtype(old_dtypes)
 
     dtype_new =dict(old_dtypes.fields)
 
-    for ad in add_dtypes:
+    # re-organize so that the un-referenced fields appear at last.
+    i=0
+    simply_add=[]
+    for nf in new_dtypes:
+        if nf[2] == "" or nf[2] == None:
+            simply_add.append(new_dtypes.pop(i))
+        else:
+            i +=1
+    new_dtypes += simply_add
+
+    for nf in new_dtypes:
         try:
-            offset = old_dtypes.fields.get(ad[3])[1]
-        except :
-            print("[load/dtype error] Can't find the field ({}) in the old dtypes".format(ad[3]))
-            raise
-        dtype_new.update({ad[0]: ((ad[1], ad[2]), offset+ad[4])})
+            # If the reference field is found.
+            offset = old_dtypes.fields.get(nf[3])[1]
+        except:
+            # If not, it could be a default field input
+            try:
+                # The "last" entry == largest offet
+                key_max = max(dtype_new, key=(lambda key: dtype_new[key][-1]))
+                off_last = dtype_new[key_max][-1]
+                size_last = dtype_new[key_max][0].itemsize
+                dtype_new.update({nf[0]: ((nf[1], nf[2]), off_last+size_last)})
+            except:
+                print("[load/dtype error] Can't find the field ({}) in the old dtypes".format(nf[3]))
+                raise
+        dtype_new.update({nf[0]: ((nf[1], nf[2]), offset+nf[4])})
 
     return dtype_new
 
 
-def get_halo_dtype(is_gal=False, double=False, read_mbp=False):
+def get_halo_dtype(is_gal=False, double=False, read_mbp=False, new_fields=None):
+    """
+    Returns dtypes needed for halo catalog.
+
+    Parameters
+    ----------
+    is_gal : False
+        If reading a galaxy catalog, additional fields are used.
+    double : False
+        NH Galaxy catalogs are in double precision due to the very deep zoom-in.
+    read_mbp : False
+        Modified version of HaloMaker provides the ID of the most bound particle
+        to be used in building trees. (In fact, it's not exactly the most bound
+        particle, but something representative of the halo dynaics)
+    new_fields : None
+        New fields can be added to the halo catalog by using the "add_dtypes" function.
+        The new field must be in the following format:
+        (name, type, shape, existing field, offset from the existing field)
+        For example,
+        ("pos", dtype_float, (3,), "x", 0),
+        ("vel", dtype_float, (3,), "vx", 0),
+        ("lvec", dtype_float, (3,), "ax", 0)
+    """
     if double:
         dtype_float = "<f8"
         df=8
@@ -63,6 +119,7 @@ def get_halo_dtype(is_gal=False, double=False, read_mbp=False):
                   ('sp', dtype_float), ('idx', '<i4'),
                   ('p_rho', dtype_float),('p_c', dtype_float),
                   ('energy', '<f8', (3,)), ('abc', '<f8', (3,))]
+                  # always 8bytes??
 
     if is_gal:
         dtype_halo += [('sig', dtype_float), ('sigbulge', dtype_float),
@@ -76,6 +133,9 @@ def get_halo_dtype(is_gal=False, double=False, read_mbp=False):
     add_dtype = [("pos", dtype_float, (3,), "x", 0),
                  ("vel", dtype_float, (3,), "vx", 0),
                  ("lvec", dtype_float, (3,), "ax", 0)]
+
+    if new_fields is not None:
+        add_dtype += new_fields
 
     return add_dtypes(dtype_halo, add_dtype)
 
