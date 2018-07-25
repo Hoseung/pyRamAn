3a8d998fc049d8c49157e6ba06bf48e96546a8bf
HS on desktop <hopung@gmail.com>
2018-07-25 17:29:27 +0900

    Analysis modules for NH

---
+++ analysis/NH_halo_match.py
@@ -0,0 +1,83 @@
+from utils import match as mtc
+import numpy as np
+
+def match_all_halos(gcat, hcat):
+    """
+    find a galaxy-halo pair.
+    """
+    from scipy.spatial import cKDTree
+
+    hkdt = cKDTree(hcat.data["pos"])
+    matched_halos = hmat.find_direct_halo(gcat.data, hcat.data, hkdt, n_match=100, debug=False)
+    gcat.data["hosthalo"] = matched_halos["id"]
+
+    return matched_halos
+
+
+def get_kd_matches(kdtree, gal, n_match=5, rscale = 4.0, dist_upper=None):
+    """
+    Allow large enough rscale so that even the most sparse galaxy will find one match.
+
+    """
+    #len_tree = kdtree.length
+    if dist_upper is None:
+        dist_upper = gal["rvir"] * rscale
+    dd, ind = kdtree.query((gal["x"],
+                            gal["y"],
+                            gal["z"]),
+                            distance_upper_bound=dist_upper,
+                            k=n_match)
+
+    ind = ind[np.isfinite(dd)]
+    dd = dd[np.isfinite(dd)]
+
+    return dd, ind
+
+def find_direct_halo(gdata, hdata, hkdt, n_match=50, debug=False):
+    sig_pos = np.std(hdata["x"])
+    sig_vel = np.std(hdata["vx"])
+
+    matched_halo = np.zeros(len(gdata), dtype=hdata.dtype)
+    matched_ids=[] # The matched_halo array is very sparse. Just keep IDs list to check for duplicates.
+    miss=0
+    isort = np.argsort(gdata["m"])[::-1]
+    for i, thisgal in enumerate(gdata[isort]):
+        r_limit = 0.8*thisgal["r"]
+        dist, i_neigh = get_kd_matches(hkdt, thisgal,
+                                       n_match=n_match,
+                                       dist_upper=r_limit)#, dist_upper=0.5/100.)
+
+        if debug:
+            print("There are {} neighbor halos within {:.3e}".format(len(dist), r_limit))
+
+        # Exclude already-matched haloes
+        i_ok = mtc.match_list_ind(hdata["id"][i_neigh],
+                                  np.setdiff1d(hdata["id"][i_neigh],
+                                               matched_ids,
+                                               assume_unique=True))
+        dist = dist[i_ok]
+        neighbor_h = hdata[i_neigh[i_ok]]
+
+        # Mass cut.
+        i_mass_ok = np.where(5*neighbor_h["mvir"] > thisgal["m"])[0]
+        if debug:
+            print("massive halos", np.log10(thisgal["m"]), i_mass_ok)
+        neighbor_h = neighbor_h[i_mass_ok]
+        dist = dist[i_mass_ok]
+        if debug :
+            print("Stellar mass {},  nei dist {}".format(neighbor_h["mvir"], dist))
+        # 6-D dist.
+        rel_vel = np.sqrt(np.einsum("...i,...i", neighbor_h["vel"] - thisgal["vel"],
+                                   neighbor_h["vel"] - thisgal["vel"]))
+
+        try:
+            matched_halo[isort[i]]=neighbor_h[np.argmin(dist * rel_vel)]
+            matched_ids.append(matched_halo[isort[i]]["id"])
+            if debug: print("Finally.. {:.3e} \n".format(matched_halo[isort[i]]["mvir"]))
+        except:
+            print(i_neigh, i_ok, i_mass_ok, dist*rel_vel, "{} Failed \n".format(thisgal["id"]))
+            miss+=1
+            pass
+    print("There are {} missing matches out of {}".format(miss, len(gdata)))
+
+    return matched_halo
---
+++ analysis/gal_props.py
@@ -0,0 +1,203 @@
+import numpy as np
+from utils import match as mtc
+
+def get_phi(rsorted,cumsum):
+    """
+    Binney Ch 2.2
+    """
+    Grav = 6.67408*1e-11*1.989*1e30/(3.0857*1e16*1e9)
+
+    pi = -Grav*cumsum/rsorted**2*np.append(rsorted[0],rsorted[1:]-rsorted[:-1])
+    return np.cumsum(pi[::-1])[::-1]
+
+
+def get_E(self, nvec=None, nvec_ys=True, ptype='star', method="Abadi2003",
+                bound_only = True, return_ellip=False):
+
+    # parameters
+    pos1, pos2, vel1, vel2 = 'z', 'x', 'vz', 'vx'
+    method="Abadi"
+
+    # constants
+    G = 6.67384e-11  # m^3 kg^-1 s^-2
+    kpc_to_m = 3.08567758e19
+    msun_in_kg = 1.9891e30 # kg
+    Grav = G * msun_in_kg/(3.0857*1e16*1e9)
+    kpc_in_cm = kpc_to_m * 1e2
+    msun_in_g = msun_in_kg * 1e3
+
+    info = self.info
+
+    cell_to_msun = info.unit_d/msun_in_g * kpc_in_cm**3
+    cell_mass = self.cell["rho"]*self.cell["dx"]**3 * cell_to_msun
+
+    r_s = np.sqrt(np.sum(np.square(self.star["pos"]), axis=1))
+    if False:
+        i_ok = np.zeros(len(r_s), dtype="bool")
+        i_ok[:] = True
+        i_ok[np.argmin(r_s)] = False
+
+        star = self.star[i_ok]
+        r_s = r_s[i_ok]
+    else:
+        star = self.star
+
+    rv = np.cross(star["pos"], star["vel"])
+    if nvec is None:
+        if nvec_ys:
+            if star["time"].mean() < 0:
+                # it's in conformal time unit.
+                from utils.cosmology import Timeconvert
+                tc = Timeconvert(info = info)
+                star['time'] = tc.time2gyr(star['time'], z_now = info.zred)
+            nvec = sum(rv[star['time']<0.01]) # younger than 10Myr
+        else:
+            R90m = get_radius(star,star['m'],0.9)
+            nvec = sum(rv[r_s<R90m])
+
+
+    # Calculating boundness requires total mass inside a radius.
+    # -> DM, Cell are also needed.
+    if hasattr(self, "cell"):
+        m_g = cell_mass
+        m_all = np.concatenate((self.star['m'],
+                                m_g,
+                                self.dm['m']))
+        r_all = np.concatenate((r_s,
+                                np.sqrt(np.sum(np.square(self.cell["pos"]), axis=1)),
+                                np.sqrt(np.sum(np.square(self.dm  ["pos"]), axis=1))))
+    else:
+        m_all = np.concatenate((self.star['m'],
+                                self.dm['m']))
+        r_all = np.concatenate((np.sqrt(np.sum(np.square(self.star["pos"]), axis=1)),
+                                np.sqrt(np.sum(np.square(self.dm  ["pos"]), axis=1))))
+
+    i_sorted = np.argsort(r_all)
+    r_all = r_all[i_sorted]
+    m_enc = np.cumsum(m_all[i_sorted])
+
+    # First nstar indices are stars.
+
+    # Exclude the star at the center.
+
+
+    bound_only = False
+
+    if bound_only:
+        i_star = i_sorted[self.bound_ptcl]
+        x  = self.star[pos1][self.bound_ptcl]
+        y  = self.star[pos2][self.bound_ptcl]
+        vx = self.star[vel1][self.bound_ptcl]
+        vy = self.star[vel2][self.bound_ptcl]
+        m  = self.star['m'] [self.bound_ptcl]
+    else:
+        #i_star = i_sorted[:len(r_s)]
+        x  = star[pos1]
+        y  = star[pos2]
+        vx = star[vel1]
+        vy = star[vel2]
+        m  = star['m']
+
+
+    #boxtokpc = self.info.pboxsize * 1000
+    i_star_dist = np.searchsorted(r_all, r_s)
+
+    if method=="scannapieco":
+        v_circ = np.sqrt(G * msun_in_kg * m_enc[i_star_dist]/
+                         (kpc_to_m * r_s)) * 1e-3 # m/s to in km/s
+        j_circ = r_s * v_circ
+        # Finally, r in code unit, v in km/s
+        j_phi = np.inner(rv, [0,1,0])
+        # Equivalent, but below is probably faster.
+        #j_phi = (x * vy - y * vx) # * boxtokpc omitted.
+        self.E = j_phi / j_circ
+    elif method=="Abadi":
+        rssort = np.argsort(r_s)
+        r_s_sorted = r_s[rssort][1:]
+
+        RV = np.inner(rv,nvec)#/np.sqrt(sum(J*J))
+        cos_alp = RV/np.sqrt((rv*rv).sum(axis=1))
+
+        st_ind = np.searchsorted(r_all, r_s[rssort])
+
+        jz = RV[rssort][1:]
+        M = m_enc[st_ind][1:]
+
+        vcir = np.sqrt(Grav*M/r_s[rssort][1:])
+        jcir = r_s[rssort][1:]*vcir
+        e = jz/jcir
+        cos = cos_alp[rssort][1:]
+
+        phi = get_phi(r_s_sorted, M)
+        specificE = phi + 0.5*(star["vel"][:,0]**2+star["vel"][:,1]**2+star["vel"][:,2]**2)[rssort][1:]
+        Ecir = 0.5*Grav*M/r_s_sorted + phi
+
+        i_sort_Ecir = np.argsort(Ecir)
+        jE = jcir[i_sort_Ecir][np.digitize(specificE,Ecir[i_sort_Ecir])-1]
+
+        try:
+            self.star["ellip"][rssort[1:]] = jz/jE
+            self.star["ellip"][rssort[0]] = 0
+        except:
+            print("[get_E] ellip field is not available")
+            self.E = jz/jE
+
+        #gal.sorted = star[rssort][1:]
+        self.e = e
+        self.cos = cos
+        self.vcir = vcir
+        self.ind_closest_star = rssort[0]
+
+
+
+def load_components(gg, thishalo, s, idlist, load_dm=True, load_cell=True):
+    import pickle
+    from utils.sampling import Region
+    from scipy.spatial import cKDTree
+    nout = s.nout
+
+    if load_dm or load_cell:
+        reg = Region()
+        reg.region_from_halo(thishalo)
+        s.set_ranges(reg.ranges)
+    else:
+        return
+
+    if load_dm:
+        s.add_part(ptypes=["dm id pos vel mass"])
+        ind = mtc.match_list_ind(s.part.dm["id"], idlist)
+        gg.dm = s.part.dm[ind]
+        gg.dm["pos"] -= gg.center_code
+        gg.dm["pos"] *= gg.info.boxtokpc
+        gg.dm["vel"] *= gg.info.kms
+        gg.dm["m"] *= gg.info.msun
+
+    if load_cell:
+        # gas properties
+        fn_cell=s.base+"/GalaxyMaker/CELL_{:05d}/CELL_{:d}_{:d}.pickle".format(nout,nout,gg.meta.id)
+        load_hydro = True
+        try:
+            gg.cell = pickle.load(open(fn_cell, "rb"))
+            if 'var5' in gg.cell.dtype.names:
+                load_hydro = False
+        except:
+            pass
+
+        if load_hydro:
+            rmin = 1e-5
+            if reg.radius < rmin:
+                reg.radius = rmin
+            s.set_ranges(reg.ranges)
+            if verbose: t0 = time.time()
+            s.add_hydro(verbose=False, load=True, pure=True)
+            if verbose:
+                t1 = time.time()
+                print("Loading hydro took", t1 - t0)
+            kdtree = cKDTree(np.stack((s.hydro.cell["x"],
+                                       s.hydro.cell["y"],
+                                       s.hydro.cell["z"])).T)
+            get_cell(s.hydro.cell, kdtree, gg, s.info)
+            #print("Retrieving cells from sim.hydro")
+            if len(gg.cell) > 1:
+                if save_cell:
+                    pickle.dump(gg.cell, open(fn_cell,"wb"))
