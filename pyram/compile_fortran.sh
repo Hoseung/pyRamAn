d570b34dfad8da5e4cb2ad3e6dddb10bf95d85e7
Hoseung Choi <hopung@gmail.com>
2020-07-28 06:36:16 +0000

    compile readr in two steps - why?

--- pyram/compile_fortran.sh
+++ pyram/compile_fortran.sh
@@ -11,4 +11,6 @@ f2py -c readhtm.f90 -m readhtm
 cd ../load
 f2py -c amr2cell_fun.f90 -m a2c
 f2py -c part_cpu_module.f90 -m part_load
-f2py -c readr.f90 -m readr
+gfortran -x f95-cpp-input -c readr.f90
+f2py -c readr.f90 -m readr --opt='-O3 -x f95-cpp-input'
+
